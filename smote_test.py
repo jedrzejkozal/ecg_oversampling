08e2c68cd263fe86085deaba1e4cbd2d6172c592
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-07-25 18:24:31 +0200

    smote tests

--- smote_test.py
+++ smote_test.py
@@ -1,14 +1,14 @@
 from dataset_utils.read_MIT_dataset import *
-from oversampling.StlBaggingGenerator import *
+from oversampling.SmoteGenerator import *
 from oversampling.reduce_imbalance import *
 from oversampling_test_base import *
 
 
-class TestStl(OversamplingTestBase):
+class TestSmote(OversamplingTestBase):
 
     @classmethod
     def get_dataset(self, num_examples):
         x_train, y_train, _, _ = load_testing_dataset()
         x_aug, y_aug = reduce_imbalance(
-            x_train, y_train, StlBaggingGenerator(), num_examples=num_examples)
+            x_train, y_train, SmoteGenerator(), num_examples=num_examples)
         return x_aug, y_aug
